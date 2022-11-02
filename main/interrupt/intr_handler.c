029fb68831ad260aa463d3eb9f3be979a7964eb4
Jackson Ming Hu <huming2207@gmail.com>
2022-11-02 23:10:37 +1100

    Get rid of stm32-cmake

--- main/interrupt/intr_handler.c
+++
@@ -1,1 +0,0 @@
-#include "intr_handler.h"
---
+++ main/stm32wlxx_it.h
@@ -0,0 +1,17 @@
+#pragma once
+
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void MemManage_Handler(void);
+void BusFault_Handler(void);
+void UsageFault_Handler(void);
+void SVC_Handler(void);
+void DebugMon_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
+void I2C2_EV_IRQHandler(void);
+void I2C2_ER_IRQHandler(void);
+void USART1_IRQHandler(void);
+void LPUART1_IRQHandler(void);
+void SUBGHZ_Radio_IRQHandler(void);
+
